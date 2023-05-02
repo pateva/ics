f0aaf06c8d51991e0da054fef8538e5141044f80
pateva <83903221+pateva@users.noreply.github.com>
2023-05-02 19:28:36 +0300

    added controller tests

--- ics/src/main/java/com/example/demo/Greeting.java
+++ ics/src/main/java/com/example/demo/Greeting.java
@@ -2,4 +2,11 @@ package com.example.demo;
 
 public record Greeting(long id, String content) {
 
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
--- ics/src/test/java/com/example/demo/DemoApplicationTests.java
+++ ics/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,52 @@
 package com.example.demo;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private HelloController controller;
+
+	private MockMvc mockMvc;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+
+	@Before
+	public void setUp() {
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+	}
+
+	@Test
+	public void testEmpty() throws Exception {
+		this.mockMvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("Welcome to localhost:8080"));
+	}
+
+	@Test
+	public void testGreeting() throws Exception {
+		String name = "petya";
+		Greeting expectedGreeting = new Greeting(1, String.format("Hello, %s!", name));
+		mockMvc.perform(get("/greeting").param("name", name))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").value(expectedGreeting.getId()))
+				.andExpect(jsonPath("$.content").value(expectedGreeting.getContent()));
 	}
 
 }
