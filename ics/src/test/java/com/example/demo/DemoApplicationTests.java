7ebf7d40919295773524a3f21224676e95332409
pateva <83903221+pateva@users.noreply.github.com>
2023-05-02 22:40:35 +0300

    replaces @Before with @BeforeEach

--- ics/src/test/java/com/example/demo/DemoApplicationTests.java
+++ ics/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,7 +27,7 @@ class DemoApplicationTests {
 		assertThat(controller).isNotNull();
 	}
 
-	@Before
+	@BeforeEach
 	public void setUp() {
 		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 	}
