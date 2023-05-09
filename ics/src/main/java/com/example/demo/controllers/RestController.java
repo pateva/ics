0df1d1b74a46c8afb2a3cef3dd9fa006d6557fbd
pateva <83903221+pateva@users.noreply.github.com>
2023-05-09 18:52:43 +0300

    adjusted the /images endpoint, not ready yet

--- ics/src/main/java/com/example/demo/controllers/RestController.java
+++ ics/src/main/java/com/example/demo/controllers/RestController.java
@@ -1,17 +1,29 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.Greeting;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicLong;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
     private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    private static final AtomicLong counter = new AtomicLong();
+    private final String API_URL = "https://api.ximilar.com/classify/v2/classify";
+    private final String API_KEY = System.getenv("API_KEY_XIMILAR");
+
 
     @GetMapping("/")
     public String empty() {
@@ -24,10 +36,36 @@ public class RestController {
     }
 
     @PostMapping("/images")
-    public String postImages(@RequestParam(value = "URL") String url) {
+    public String postImages(@RequestParam(value = "URL") String imageUrl) throws IOException {
 
-        return "test";
+        String json = "{ \"task_id\": " + counter.incrementAndGet() + ", \"data\": { \"image_url\": \"" + imageUrl + "\" } }";
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(API_URL + "?url=" + imageUrl);
+        httpGet.setHeader("Authorization", "Token " + API_KEY);
+
+//        CloseableHttpResponse httpResponse = null;
+//        try {
+//            httpResponse = httpClient.execute(httpGet);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            HttpEntity responseEntity = (HttpEntity) httpResponse.getEntity();
+//            String responseString = EntityUtils.toString((org.apache.hc.core5.http.HttpEntity) responseEntity);
+//
+//            JSONObject jsonResponse = new JSONObject(responseString);
+//            return jsonResponse.toString();
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        } finally {
+//            httpResponse.close();
+//        }
+
+        return "not ready yet";
     }
 
-
 }
+
+
+
--- ics/src/main/java/com/example/demo/repositories/BaseRepository.java
+++ ics/src/main/java/com/example/demo/repositories/BaseRepository.java
@@ -1,12 +1,6 @@
 package com.example.demo.repositories;
 
 public abstract class BaseRepository {
-    private final String API_URL;
-    private final String API_KEY;
 
-    protected BaseRepository() {
-        API_URL = "https://api.ximilar.com/classify/v2/classify";
-        API_KEY = System.getenv("API_KEY_XIMILAR");
-    }
 
 }
