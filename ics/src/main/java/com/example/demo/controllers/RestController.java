b534080493094f11e0566db1b70b151a1a3c420d
pateva <83903221+pateva@users.noreply.github.com>
2023-05-10 12:10:12 +0300

    stash

--- ics/src/main/java/com/example/demo/controllers/RestController.java
+++ ics/src/main/java/com/example/demo/controllers/RestController.java
@@ -8,10 +8,12 @@ import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.json.JSONObject;
-import org.springframework.http.HttpEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicLong;
@@ -21,13 +23,15 @@ public class RestController {
 
     private static final String template = "Hello, %s!";
     private static final AtomicLong counter = new AtomicLong();
-    private final String API_URL = "https://api.ximilar.com/classify/v2/classify";
+    private final String API_URL_CLASSIFY = "https://api.ximilar.com/classify/v2/classify";
     private final String API_KEY = System.getenv("API_KEY_XIMILAR");
 
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/")
     public String empty() {
-        return new String("Welcome to localhost:8080");
+        return new String("Welcome!!!:)");
     }
 
     @GetMapping("/greeting")
@@ -36,33 +40,18 @@ public class RestController {
     }
 
     @PostMapping("/images")
-    public String postImages(@RequestParam(value = "URL") String imageUrl) throws IOException {
+    public ResponseEntity<String> classifyImage(@RequestParam("url") String imageUrl) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Token " + API_KEY);
 
-        String json = "{ \"task_id\": " + counter.incrementAndGet() + ", \"data\": { \"image_url\": \"" + imageUrl + "\" } }";
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet(API_URL + "?url=" + imageUrl);
-        httpGet.setHeader("Authorization", "Token " + API_KEY);
+        JSONObject body = new JSONObject();
+        body.put("url", imageUrl);
 
-//        CloseableHttpResponse httpResponse = null;
-//        try {
-//            httpResponse = httpClient.execute(httpGet);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        try {
-//            HttpEntity responseEntity = (HttpEntity) httpResponse.getEntity();
-//            String responseString = EntityUtils.toString((org.apache.hc.core5.http.HttpEntity) responseEntity);
-//
-//            JSONObject jsonResponse = new JSONObject(responseString);
-//            return jsonResponse.toString();
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        } finally {
-//            httpResponse.close();
-//        }
+        HttpEntity<String> requestEntity = new HttpEntity<>(body.toString(), headers);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(API_URL_CLASSIFY, HttpMethod.POST, requestEntity, String.class);
 
-        return "not ready yet";
+        return new ResponseEntity<>(responseEntity.getBody(), HttpStatus.OK);
     }
 
 }
