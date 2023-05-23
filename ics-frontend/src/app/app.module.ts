fe5b25d19eb00e2886c483b4113d3a799812b96f
pateva <83903221+pateva@users.noreply.github.com>
2023-05-23 15:43:37 +0300

    at least the "correct"colours are visible

--- ics-frontend/src/app/app.module.ts
+++ ics-frontend/src/app/app.module.ts
@@ -19,7 +19,8 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
