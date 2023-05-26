768509ded4407d05d425aed7851aeb89d580b385
pateva <83903221+pateva@users.noreply.github.com>
2023-05-26 23:43:03 +0300

    small adjustment

--- ics-frontend/src/app/app.module.ts
+++ ics-frontend/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
