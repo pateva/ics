1026abe8012cd411e1aa7a98d2337e9f9d299758
pateva <83903221+pateva@users.noreply.github.com>
2023-05-28 19:51:08 +0300

    first rest assured test

---
+++ ics/src/test/java/restAssuredControllersTest/JSONSchemaValidator.java
@@ -0,0 +1,34 @@
+package restAssuredControllersTest;
+
+import org.junit.jupiter.api.Test;
+
+public class JSONSchemaValidator {
+    @Test
+    void responseParse() {
+//        //base URI with Rest Assured class
+//        RestAssured.baseURI = "https://run.mocky.io/v3";
+//
+//        //input details
+//        RequestSpecification h = RestAssured.given();
+//
+//        //get response
+//        Response res = h.get("/a1b7b64c-0204-409a-aa0c-e8314a5ddabf");
+//
+//        //Response body
+//        ResponseBody b = res.getBody();
+//
+//        //convert response body to string
+//        String responseBody = b.asString();
+//
+//        //JSON Representation from Response Body
+//        JsonPath jsnPath = res.jsonPath();
+//
+//        //Get value of Location Key
+//        String s = jsnPath.get("student");
+//        System.out.println("Course name: " + s);
+//        String std = jsnPath.get("standard");
+//        System.out.println("Standard: " + std);
+//        String m = jsnPath.get("marks");
+//        System.out.println("Marks: " + m);
+    }
+}
---
+++ ics/src/test/java/restAssuredControllersTest/TestArguments.java
@@ -0,0 +1,9 @@
+package restAssuredControllersTest;
+
+public class TestArguments {
+    private TestArguments() {}
+
+    public static final String BASE_URL = "http://localhost:8080";
+    public static final String PATH = "/images";
+
+};
