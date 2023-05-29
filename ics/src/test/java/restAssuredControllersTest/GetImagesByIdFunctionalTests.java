7ecda523fc0dc462dab6eb704d72e10693f8d4fb
pateva <83903221+pateva@users.noreply.github.com>
2023-05-29 11:11:37 +0300

    images by id functional tests

--- ics/src/test/java/restAssuredControllersTest/GetImagesByIdFunctionalTests.java
+++ ics/src/test/java/restAssuredControllersTest/GetImagesByIdFunctionalTests.java
@@ -1,9 +1,38 @@
 package restAssuredControllersTest;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+
+import static io.restassured.RestAssured.given;
+import static restAssuredControllersTest.TestArguments.BASE_URL;
+import static restAssuredControllersTest.TestArguments.PATH_ID;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class GetImagesByIdFunctionalTests {
+    private static RequestSpecification requestSpecification;
+
+    @BeforeAll
+    static void setUp() {
+        requestSpecification = new RequestSpecBuilder()
+                .setBaseUri(BASE_URL)
+                .setBasePath(PATH_ID)
+                .build();
+    }
+
+    @Test
+    @DisplayName("Get with path variable --ValidId returns 200")
+    void testGetWithId_Returns200() {
+        given()
+                .spec(requestSpecification)
+                .pathParam("id", "6")
+                .when()
+                .get()
+                .prettyPeek()
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
 
 }
--- ics/src/test/java/restAssuredControllersTest/TestArguments.java
+++ ics/src/test/java/restAssuredControllersTest/TestArguments.java
@@ -5,6 +5,7 @@ public class TestArguments {
 
     public static final String BASE_URL = "http://localhost:8080";
     public static final String PATH = "/images";
+    public static final String PATH_ID = PATH + "/{id}";
 
     public static final String IMAGES_JSON_TEMPLATE_PATH = "src/test/resources/json-templates/images-template.json";
 
