462a1c4efdd86665bc9f2fb620d18bbbe02b2876
pateva <83903221+pateva@users.noreply.github.com>
2023-06-03 10:04:58 +0300

    merge main to current branch

--- ics-frontend/src/app/data/data.service.ts
+++ ics-frontend/src/app/data/data.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { PostRequestBody } from '../interfaces/postRequestBody';
+import { ImageClassificationResponse } from '../interfaces/imageClassificationResponse';
 
 @Injectable({
   providedIn: 'root'
