7471ca1313866e751021034bcd899371945f9ce2
pateva <83903221+pateva@users.noreply.github.com>
2023-06-04 10:24:42 +0300

    halfway with the autocomplete

--- ics-frontend/src/app/data/data.service.ts
+++ ics-frontend/src/app/data/data.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { PostRequestBody } from '../interfaces/postRequestBody';
 import { ImageClassificationResponse } from '../interfaces/imageClassificationResponse';
+import { Label } from '../interfaces/labelResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,14 @@ import { ImageClassificationResponse } from '../interfaces/imageClassificationRe
 export class DataService {
 
   baseUrl = 'http://localhost:8080/images';
+  labelUrl = 'http://localhost:8080/labels';
 
   constructor(private http: HttpClient) { }
 
   postImageUrl(imageUrl: string): Observable<any> {
 
     return this.http.post<ImageClassificationResponse>(this.baseUrl, this.postImageRequestBody(imageUrl));
-    
+
   }
 
   postImageRequestBody(imageUrl: string): PostRequestBody {
@@ -31,16 +33,20 @@ export class DataService {
     return requestBody;
   }
 
-  getImageById(imageId: number) : Observable<ImageClassificationResponse> {
-    return this.http.get<ImageClassificationResponse>( this.baseUrl + `/${imageId}`);
+  getImageById(imageId: number): Observable<ImageClassificationResponse> {
+    return this.http.get<ImageClassificationResponse>(this.baseUrl + `/${imageId}`);
   }
 
-  getAllImages() : Observable<ImageClassificationResponse[]> {
+  getAllImages(): Observable<ImageClassificationResponse[]> {
     return this.http.get<ImageClassificationResponse[]>(this.baseUrl);
   }
 
-  getImagesByLabels(labels: string[]) : Observable<ImageClassificationResponse[]> {
+  getImagesByLabels(labels: string[]): Observable<ImageClassificationResponse[]> {
     const queryParams = labels.map(label => `labels=${encodeURIComponent(label)}`).join('&');
     return this.http.get<ImageClassificationResponse[]>(this.baseUrl + `?${queryParams}`);
-}
+  }
+
+  getAllLabels():Observable<Label[]> {
+    return this.http.get<Label[]>(this.labelUrl);
+  }
 }
